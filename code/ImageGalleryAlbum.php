87c325c2d63995b272a13bcbaca0860458dd274d
Ayush kumar jha <ayushkmrjha@yahoo.com>
2023-10-25 00:02:19 +0530

    Update ImageGalleryAlbum.php
    1. Added use statements for better namespace handling.
    2. Changed FieldSet to FieldList for form fields.
    3. Replaced ImageUploadField with UploadField for the cover  image to follow current best practices.
    4.Utilized method chaining for field creation in getCMSFields.
    5. Updated the LinkingMode method to use getRequest() for parameter retrieval.
    6. Simplified the ImageCount method by using a filter and count.
    7. Made onBeforeWrite and onBeforeDelete methods protected instead of public.
    8. Removed unnecessary comments, and improved code readability and formatting.

--- code/ImageGalleryAlbum.php
+++ code/ImageGalleryAlbum.php
@@ -1,85 +1,92 @@
 <?php
 
+use SilverStripe\CMS\Model\Folder;
+use SilverStripe\CMS\Controllers\Controller;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+
 class ImageGalleryAlbum extends DataObject
 {
-	static $db = array (
-		'AlbumName' => 'Varchar(255)',
-		'Description' => 'Text'
-	);
-	
-	static $has_one = array (
-		'CoverImage' => 'Image',
-		'ImageGalleryPage' => 'ImageGalleryPage',
-		'Folder' => 'Folder'
-	);
-	
-	static $has_many = array (
-		'GalleryItems' => 'ImageGalleryItem'
-	);
-	
+    private static $db = [
+        'AlbumName' => 'Varchar(255)',
+        'Description' => 'Text',
+    ];
 
-	
-	public function getCMSFields_forPopup()
-	{
-		return new FieldSet(
-			new TextField('AlbumName', _t('ImageGalleryAlbum.ALBUMTITLE','Album Title')),
-			new TextareaField('Description', _t('ImageGalleryAlbum.DESCRIPTION','Description')),
-			new ImageUploadField('CoverImage',_t('ImageGalleryAlbum.COVERIMAGE','Cover Image'))
-		);
-	}
-	
-	public function Link()
-	{
-		$name = $this->Folder()->Name;
-		if(!$name) {
-			$name = $this->FolderID;
-		}
-		return $this->ImageGalleryPage()->Link('album/'.$name);
-	}
-	
-	public function LinkingMode()
-	{
-		return Controller::curr()->urlParams['ID'] == $this->Folder()->Name ? "current" : "link";
-	}
-	
-	public function ImageCount()
-	{
-		$images = DataObject::get("ImageGalleryItem","AlbumID = {$this->ID}"); 
-		return $images ? $images->Count() : 0;
-	}
-	
-	public function FormattedCoverImage()
-	{
-		return $this->CoverImage()->CroppedImage($this->ImageGalleryPage()->CoverImageWidth,$this->ImageGalleryPage()->CoverImageHeight);
-	}
-	
-	function onBeforeWrite()
-	{
-		parent::onBeforeWrite();
-		if(isset($_POST['AlbumName'])) {
-		  $clean_name = SiteTree::generateURLSegment($_POST['AlbumName']);
-			if($this->FolderID) {
-				$this->Folder()->setName($clean_name);
-				$this->Folder()->Title = $clean_name;
+    private static $has_one = [
+        'CoverImage' => Image::class,
+        'ImageGalleryPage' => ImageGalleryPage::class,
+        'Folder' => Folder::class,
+    ];
 
-				$this->Folder()->write();
-			}
-			else {
-				$folder = Folder::findOrMake('image-gallery/'.$this->ImageGalleryPage()->RootFolder()->Name.'/'.$clean_name);
-				$this->FolderID = $folder->ID;
-			}
-		}
-	}
-	
-	function onBeforeDelete()
-	{
-		parent::onBeforeDelete();
-		$this->GalleryItems()->removeAll();
-		$this->Folder()->delete();
-	}
-	
-	
+    private static $has_many = [
+        'GalleryItems' => ImageGalleryItem::class,
+    ];
+
+    public function getCMSFields()
+    {
+        $fields = FieldList::create(
+            TextField::create('AlbumName', _t('ImageGalleryAlbum.ALBUMTITLE', 'Album Title')),
+            TextareaField::create('Description', _t('ImageGalleryAlbum.DESCRIPTION', 'Description')),
+            UploadField::create('CoverImage', _t('ImageGalleryAlbum.COVERIMAGE', 'Cover Image'))
+        );
+
+        return $fields;
+    }
+
+    public function Link()
+    {
+        $folder = $this->Folder();
+        $name = $folder ? $folder->Name : $this->FolderID;
+
+        return $this->ImageGalleryPage()->Link("album/$name");
+    }
+
+    public function LinkingMode()
+    {
+        return Controller::curr()->getRequest()->param('ID') == $this->Folder()->Name ? 'current' : 'link';
+    }
+
+    public function ImageCount()
+    {
+        return ImageGalleryItem::get()->filter(['AlbumID' => $this->ID])->count();
+    }
+
+    public function FormattedCoverImage()
+    {
+        return $this->CoverImage()->CroppedImage(
+            $this->ImageGalleryPage()->CoverImageWidth,
+            $this->ImageGalleryPage()->CoverImageHeight
+        );
+    }
+
+    protected function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+
+        if (isset($_POST['AlbumName'])) {
+            $clean_name = SiteTree::generateURLSegment($_POST['AlbumName']);
+            if ($this->FolderID) {
+                $folder = $this->Folder();
+                $folder->setName($clean_name);
+                $folder->Title = $clean_name;
+                $folder->write();
+            } else {
+                $folder = Folder::findOrMake("image-gallery/{$this->ImageGalleryPage()->RootFolder()->Name}/$clean_name");
+                $this->FolderID = $folder->ID;
+            }
+        }
+    }
+
+    protected function onBeforeDelete()
+    {
+        parent::onBeforeDelete();
+        $this->GalleryItems()->removeAll();
+        $folder = $this->Folder();
+        if ($folder) {
+            $folder->delete();
+        }
+    }
 }
-
-
-?>
